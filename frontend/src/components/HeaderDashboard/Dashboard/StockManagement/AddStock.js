2d234ebc1bfe148cc20989a0af3eb8091c552f8a
Prasoon2050 <2020kucp1066@iiitkota.ac.in>
2023-09-30 12:57:01 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ frontend/src/components/HeaderDashboard/Dashboard/StockManagement/AddStock.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import "./AddStock.css";
+import axios from "axios";
+
+const AddStock = ({ onStockUpdate }) => {
+  const token = localStorage.getItem("token");
+  const { register, handleSubmit, reset } = useForm();
+
+  const [stockInfo, setstockInfo] = useState();
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState(""); 
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await fetch("http://localhost:5000/api/admin/stock", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(data),
+      });
+  
+      if (response.ok) {
+        const responseData = await response.json();
+        setMessage(responseData.message);
+        setMessageType("success");
+        onStockUpdate();
+        reset(); 
+      } else {
+        const responseData = await response.json();
+        setMessage(responseData.message);
+        setMessageType("error");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setMessage("An error occurred");
+      setMessageType("error");
+    }
+  };
+
+  return (
+    <div className="add-stock-container">
+      <h2>Add Stock</h2>
+      {message && <div className={`message ${messageType}`}>{message}</div>}
+      <form onSubmit={handleSubmit(onSubmit)} className="add-stock-form">
+        <input
+          type="text"
+          id="productName"
+          className="add-stock-input-field"
+          placeholder="Enter Product Name"
+          {...register("productName", {
+            required: "Enter Product Name",
+          })}
+        />
+        <input
+          type="number"
+          id="productQuantity"
+          className="add-stock-input-field"
+          placeholder="Enter Product Quantity"
+          {...register("productQuantity", {
+            required: "Enter Product Quantity",
+          })}
+        />
+        <input
+          type="number"
+          id="costPrice"
+          className="add-stock-input-field"
+          placeholder="Enter Cost Price"
+          {...register("costPrice", {
+            required: "Enter Cost Price",
+          })}
+        />
+        <input
+          type="number"
+          id="sellingPrice"
+          className="add-stock-input-field"
+          placeholder="Enter Selling Price"
+          {...register("sellingPrice", {
+            required: "Enter Selling Price",
+          })}
+        />
+        <button type="submit" className="add-stock-submit-button">
+          Add Stock
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddStock;
---
+++ frontend/src/components/HeaderDashboard/HeaderDashboard.js
@@ -0,0 +1,93 @@
+// Dashboard.js
+import Navbar from "./Dashboard/Navbar/Navbar";
+import EmployeeManagement from "./Dashboard/EmployeeManagement/EmployeeManagement";
+import StockManagement from "./Dashboard/StockManagement/StockManagement";
+import SalesHistory from "./Dashboard/SalesHistory/SalesHistory";
+import Billing from "./Dashboard/Billing/Billing";
+import Profile from "./Dashboard/Profile/Profile";
+import "./styles.css";
+
+import React, { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
+import axios from "axios";
+
+function HeaderDashboard() {
+	const [userData, setUserData] = useState(null);
+	const [selectedComponent, setSelectedComponent] = useState("billing");
+
+	const token = localStorage.getItem("token");
+	useEffect(() => {
+		if (token) {
+			const decoded = jwt_decode(token);
+			const role = decoded.role;
+			// console.log(role);
+
+			axios
+				.get(`http://localhost:5000/api/${role}/profile`, {
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				})
+				.then((response) => {
+					setUserData(response.data);
+				})
+				.catch((error) => {
+					// Handle different types of errors
+					if (error.response) {
+						// The request was made and the server responded with a status code
+						console.error(
+							"Error response from server:",
+							error.response
+						);
+					} else if (error.request) {
+						// The request was made but no response was received
+						console.error(
+							"No response from server:",
+							error.request
+						);
+					} else {
+						// Something happened in setting up the request
+						console.error("Request setup error:", error.message);
+					}
+				});
+		}
+	}, [token]);
+	const decoded = jwt_decode(token);
+	const role = decoded.role;
+
+	const renderComponent = () => {
+		switch (selectedComponent) {
+			case "profile":
+				return <Profile role={role} />;
+			case "employee-management":
+				return <EmployeeManagement />;
+			case "stock-management":
+				return <StockManagement role={role} />;
+			case "sales-history":
+				return <SalesHistory role={role} />;
+			case "billing":
+				return <Billing role={role} />;
+			default:
+				return null;
+		}
+	};
+
+	return (
+		<div>
+			<div>
+				<div className="header-dashboard">
+					<div className="navbar-container">
+						<Navbar
+							selectComponent={selectedComponent}
+							onSelectComponent={setSelectedComponent}
+							role={role}
+						/>
+					</div>
+					<div className="dashboard">{renderComponent()}</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default HeaderDashboard;
